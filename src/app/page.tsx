d8687dcffb1e4e3a6fc410843aae25791a9ae9cb
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-04-10 22:33:52 +0800

    style: float theme toggle on mobile, adjust page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TechStackSection from '@/components/TechStackSection'
 
 export default function Home() {
     return (
-        <div className="mx-auto px-10 flex flex-col min-h-screen items-center bg-background font-sans dark:bg-navy max-w-5xl">
+        <div className="mx-auto md:px-10 px-4 flex flex-col min-h-screen items-center bg-background font-sans dark:bg-navy max-w-5xl">
             <Navbar />
             <HeroSection />
             <TechStackSection />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
     return (
-        <div className="flex items-center justify-between w-full font-mono text-blue-strong py-6 mb-10">
-            <nav>
-                <ul className="flex gap-8 dark:text-sky">
+        <header className="w-full font-mono">
+            <nav className="flex items-center justify-center sm:justify-start w-full text-xs md:text-base font-mono text-blue-strong py-6 mb-10">
+                <ul className="flex gap-4 md:gap-8 dark:text-sky">
                     <li className="hover:underline underline-offset-4 decoration-2">
                         <a href="#home">// home</a>
                     </li>
@@ -19,8 +19,11 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
-            <ThemeToggle />
-        </div>
+
+            <div className="fixed top-4 right-4 z-50 text-blue dark:text-sky">
+                <ThemeToggle />
+            </div>
+        </header>
     )
 }
 
